dcb4f2cf6b361afdc49bc23a8b70bbae5bc20e13
Thiane Milena <milenathiane7@gmail.com>
2024-04-17 13:30:02 -0300

    Implementação do modal de feedback para requisições

--- pages/cadastro_parceiros/cadastro_parceiros.js
+++ pages/cadastro_parceiros/cadastro_parceiros.js
@@ -1,28 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.form').addEventListener("submit", function(event) {
-        event.preventDefault();
-
-        var formData = new FormData(this);
-
-        fetch(this.action, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                alert("Mensagem enviada com sucesso!");
-
-                document.querySelector('.form').reset();
-            } else {
-                alert("Houve um erro ao enviar a mensagem. Por favor, tente novamente."); 
-            }
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-            alert("Houve um erro ao enviar a mensagem. Por favor, tente novamente."); 
-        });
+      event.preventDefault();
+  
+      var formData = new FormData(this);
+  
+      fetch(this.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          document.getElementById("success-modal").style.display = "block";
+          document.querySelector('.form').reset();
+        } else {
+          document.getElementById("error-modal").style.display = "block";
+        }
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+        document.getElementById("error-modal").style.display = "block";
+      });
     });
-});
+    
+    var closeButtons = document.querySelectorAll('.close');
+    closeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        this.parentElement.parentElement.style.display = "none";
+      });
+    });
+ });
+  
