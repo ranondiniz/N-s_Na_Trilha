1e5c394b3eb4077924570036d99e9263411620c7
Thiane Milena <milenathiane7@gmail.com>
2024-04-22 10:32:43 -0300

    Ajustes no modal de requisições

--- pages/cadastro_parceiros/cadastro_parceiros.js
+++ pages/cadastro_parceiros/cadastro_parceiros.js
@@ -1,35 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.form').addEventListener("submit", function(event) {
+  document.querySelector('.form').addEventListener("submit", function(event) {
       event.preventDefault();
   
       var formData = new FormData(this);
   
       fetch(this.action, {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Accept': 'application/json'
-        }
+          method: 'POST',
+          body: formData,
+          headers: {
+              'Accept': 'application/json'
+          }
       })
       .then(response => {
-        if (response.ok) {
-          document.getElementById("success-modal").style.display = "block";
-          document.querySelector('.form').reset();
-        } else {
-          document.getElementById("error-modal").style.display = "block";
-        }
+          if (response.ok) {
+              document.getElementById("success-modal").style.display = "block";
+              document.querySelector('.form').reset();
+          } else {
+              document.getElementById("error-modal").style.display = "block";
+          }
       })
       .catch(error => {
-        console.error('Erro:', error);
-        document.getElementById("error-modal").style.display = "block";
+          console.error('Erro:', error);
+          document.getElementById("error-modal").style.display = "block";
       });
-    });
-    
-    var closeButtons = document.querySelectorAll('.close');
-    closeButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        this.parentElement.parentElement.style.display = "none";
-      });
-    });
- });
+  });
   
+  var okButtons = document.querySelectorAll('.ok-button');
+  okButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+          this.parentElement.parentElement.style.display = "none";
+      });
+  });
+});
